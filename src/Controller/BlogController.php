6776519cc87563edad540b9837b07f887b101bca
rand0mdev <bechirr71@gmail.com>
2019-07-27 09:08:37 +0000

    [Blog] add article details page

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,17 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends AbstractController
 {
-    /**
-     * @Route("/blog", name="blog")
-     */
     public function index()
     {
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
         ]);
+    }
+
+    public function show(): Response
+    {
+        return $this->render('blog/show.html.twig');
     }
 }
