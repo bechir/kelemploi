322950cf997222694ebc62cb51a2a9c455e9a321
rand0mdev <bechiirr71@gmail.com>
2019-11-10 03:26:35 +0000

    Add frequent search

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Industry;
 use App\Entity\JobCategory;
 use App\Entity\Region;
 use App\Form\ContactType;
@@ -31,7 +32,64 @@ class HomeController extends Controller
 
     public function frequentSearch(): Response
     {
-        return $this->render('home/frequent-search.html.twig');
+        $regions = $this->getDoctrine()->getRepository(Region::class)->findAll();
+        $jobCategories = $this->getDoctrine()->getRepository(JobCategory::class)->findAll();
+        $industries = $this->getDoctrine()->getRepository(Industry::class)->findAll();
+
+        $em = $this->getDoctrine()->getManager();
+        $q =  $em->createQueryBuilder();
+        
+        $regionCounts = $q->select('count(a.id), r.slug as region_slug', 'a')
+            ->from('App:Application', 'a')
+            ->leftJoin('a.company', 'c')
+                ->addSelect('c')
+            ->leftJoin('c.region', 'r')
+                ->addSelect('r')
+            ->groupBy('r.name')
+            ->getQuery()
+            ->getArrayResult();
+
+        $categoryCounts = $em->createQueryBuilder()
+            ->select('count(a.id), c.slug as category_slug', 'a')
+            ->from('App:Application', 'a')
+            ->leftJoin('a.postCategory', 'c')
+                ->addSelect('c')
+            ->groupBy('c.name')
+            ->getQuery()
+            ->getArrayResult();
+
+        $industryCounts = $em->createQueryBuilder()
+            ->select('count(a.id), i.slug as industry_slug', 'a')
+            ->from('App:Application', 'a')
+            ->leftJoin('a.company', 'c')
+                ->addSelect('c')
+            ->leftJoin('c.industry', 'i')
+                ->addSelect('i')
+            ->groupBy('i.name')
+            ->getQuery()
+            ->getArrayResult();
+
+        $keysValuesRegion = [];
+        $keysValuesJobCategory = [];
+        $keysValuesIndustry = [];
+        foreach ($regionCounts as $c) {
+            $keysValuesRegion[$c['region_slug']] = $c[1];
+        }
+        foreach ($categoryCounts as $c) {
+            $keysValuesJobCategory[$c['category_slug']] = $c[1];
+        }
+        foreach ($industryCounts as $c) {
+            $keysValuesIndustry[$c['industry_slug']] = $c[1];
+        }
+
+        return $this->render('home/frequent-search.html.twig', [
+            'regions' => $regions,
+            'region_counts' => $keysValuesRegion,
+            'job_categories' => $jobCategories,
+            'job_category_counts' => $keysValuesJobCategory,
+            'industries' => $industries,
+            'industry_counts' => $keysValuesIndustry,
+        ]);
     }
 
     public function regionsListing(): Response
--- src/Entity/Industry.php
+++ src/Entity/Industry.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\IndustryRepository")
@@ -20,6 +21,12 @@ class Industry
      * @ORM\Column(type="string", length=255)
      */
     private $name;
+
+    /**
+     * @Gedmo\Slug(fields={"name"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
 
     public function getId(): ?int
     {
@@ -41,5 +48,17 @@ class Industry
     public function __toString()
     {
         return $this->name;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
     }
 }
